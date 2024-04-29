ca535ef786dcc65b241a7b65ff18e305123d9a00
UjjawalChoudhary912 <ujjawalchoudhary912@gmail.com>
2024-04-29 13:19:21 +0530

    Header is set with heading and toggle button

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import {nanoid} from 'nanoid';
 import NotesList from "./components/NotesList";
 import Search from "./components/Search";
+import Header from "./components/Header";
 
 
 const App = () => {
@@ -30,6 +31,8 @@ const App = () => {
 
      const [searchText, setSearchText] = useState('');
 
+     const [darkMode, setDarkMode] = useState(false);
+
      const addNote = (text) =>{
         const date = new Date();
         const newNote ={
@@ -48,12 +51,15 @@ const App = () => {
     }
 
   return (
-    <div className="container">
+    <div className={`${darkMode && 'dark-mode'}`}>
+      <div className="container">
+      <Header handleToggleDarkMode={setDarkMode}/>
       <Search handleSearchNote={setSearchText}/>
       <NotesList notes={notes.filter((note) => note.text.toLowerCase().includes(searchText))} 
       handleAddNote={addNote}
       handleDeleteNote ={deleteNote}
       />
+    </div>
     </div>
   )
 }
